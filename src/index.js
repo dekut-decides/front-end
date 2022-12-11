fd6f9a08209ddd8012f95cfba1e7853fa96a5120
telvinrunoh@gmail.com <telvinrunoh>
2022-12-11 14:03:16 +0300

    added both browserrouter and hashrouter

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ import { BrowserRouter,HashRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <BrowserRouter> */}
+    <BrowserRouter>
     <HashRouter>
         <App />
     </HashRouter>
-    {/* </BrowserRouter> */}
+    </BrowserRouter>
   </React.StrictMode>
 );
 

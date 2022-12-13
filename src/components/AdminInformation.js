7b386c95f4affb8916d8105ffeeb1e914bf7a981
telvinrunoh@gmail.com <telvinrunoh>
2022-12-13 09:45:17 +0300

    added the admin info.js

--- src/components/AdminInformation.js
+++ src/components/AdminInformation.js
@@ -9,38 +9,38 @@ function AdminInformation() {
           <div className="admin-information-header">
             <h2>INSTRUCTIONS</h2>
           </div>
-          <h2>Welcome</h2>
-          <h4>
+          <h3>Welcome</h3>
+          <h5>
            The voting process is divided into three phases.
            You as the admin control these phases.voters have to 
            participate in accordance to the ongoing phase that is
            set.Below are some of the steps you as the admin MUST 
            follow in order to execute a successful election.
-          </h4>
+          </h5>
 
           <ol className="orderedList">
             <li>
-              <h3>
+              <h4>
                 After the smart contract is deployed, click on the add candidates section to add the election aspirants one by one
-              </h3>
+              </h4>
             </li>
-            <h3>
+            <h4>
               <li>Once all the candidates have been added,click on the change state section and select the registration phase</li>
-            </h3>
+            </h4>
             <li>
-              <h3>
+              <h4>
                 Click on the register voter section and add each voter address to allow them to vote
-              </h3>
+              </h4>
             </li>
             <li>
-              <h3>
+              <h4>
                 After all the voters have been registered, click on the change state section and select the voting phase
-              </h3>
+              </h4>
             </li>
             <li>
-              <h3>
+              <h4>
                 To end the election, click on the change state section and select the results phase.This will display the election results to voters
-              </h3>
+              </h4>
             </li>
           </ol>
         </div>
